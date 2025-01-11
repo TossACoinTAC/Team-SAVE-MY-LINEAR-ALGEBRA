30c2ed793f96d17b7f54595ce1699aa1ce2a836c
天空を駆ける風の都 <yingbipao@gmail.com>
2025-01-11 20:31:15 +0800

    updated bgm

--- Scripts/GameManagers/GameManager.py
+++ Scripts/GameManagers/GameManager.py
@@ -270,6 +270,7 @@ class GameManager:
                     exit()
                 case Events.GAME_WIN:
                     self.active_scene = Scenes.GAMEWIN
+                    self.bgm_player.stop()
 
                 case Events.RESTART:
                     self.__init__()
@@ -277,6 +278,7 @@ class GameManager:
                 case Events.MAIN_TO_STARTROOM:
                     self.active_scene = Scenes.START_ROOM
                     self.bgm_player.stop()
+                    self.bgm_player.play("COMMON", -1)
 
                 case Events.ROOM_CLEAR:
                     self.room.open_doors()
